479293389049e53da56a78c2a935ed1eb2678b29
niclassandberg <niclassnittsandberg@gmail.com>
2022-03-18 19:26:03 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/anders-app/precache-manifest.baec24e6c1726fd4bfee338a2ed30f23.js"
+  "/anders-app/precache-manifest.73dd044fe332284ca410923062a7e54a.js"
 );
 
 self.addEventListener('message', (event) => {
